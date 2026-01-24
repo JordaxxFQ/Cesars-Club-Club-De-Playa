62b345e62a452c07eaa8e7cc8dd5f1797ba0a610
¨ismaelofuentes¨ <ismaelofuentes@gmail.com>
2026-01-24 14:16:51 -0400

    push

--- Cesars Club Club De Playa/Formularios/Form3.Designer.vb
+++ Cesars Club Club De Playa/Formularios/Form3.Designer.vb
@@ -22,9 +22,64 @@ Partial Class Form3
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form3"
+        Button1 = New Button()
+        Button2 = New Button()
+        Label1 = New Label()
+        TextBox1 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(16, 35)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(33, 23)
+        Button1.TabIndex = 0
+        Button1.Text = "-"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(120, 35)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(30, 23)
+        Button2.TabIndex = 1
+        Button2.Text = "+"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(55, 39)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(59, 15)
+        Label1.TabIndex = 2
+        Label1.Text = "AGREGAR"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(16, 74)
+        TextBox1.Name = "TextBox1"
+        TextBox1.ReadOnly = True
+        TextBox1.Size = New Size(134, 23)
+        TextBox1.TabIndex = 3
+        ' 
+        ' Form3
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(185, 142)
+        Controls.Add(TextBox1)
+        Controls.Add(Label1)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Name = "Form3"
+        Text = "Form3"
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
 End Class
