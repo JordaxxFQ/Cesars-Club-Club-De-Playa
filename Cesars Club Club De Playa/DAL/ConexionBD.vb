66e4d6449b1232210d8f03db5ecc550528efb258
JordaxxFQ <jordan7frontado@gmail.com>
2026-01-24 20:03:07 +0100

    conexion bd

--- Cesars Club Club De Playa/DAL/ConexionBD.vb
+++
@@ -1,6 +0,0 @@
-﻿Imports System.Data.OleDb
-
-
-Public Class ConexionBD
-    Dim yo As String = "hola"
-End Class
---
+++ Cesars Club Club De Playa/DAL/Module1.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Data
+Imports System.Data.OleDb
+
+Module Module1
+    Public conexion As New OleDbConnection
+    Public estado As String
+    Public comando As New oledbcommand
+
+    Sub enlace()
+        Try
+            conexion.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Cesars Club Club De Playa\CesarsClub.accdb"
+            conexion.Open()
+            estado = "Conectado"
+        Catch ex As Exception
+            estado = "Desconectado"
+        End Try
+    End Sub
+
+
+End Module
