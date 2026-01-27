b20a4461b9a0d8739ba7f8c6ccd6ec717cd7cdf9
JordaxxFQ <jordan7frontado@gmail.com>
2026-01-27 17:16:19 +0100

    new form

--- Cesars Club Club De Playa/Formularios/Form3.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class Form3
-    Private Sub Form3_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
-        Application.Exit()
-    End Sub
-End Class
