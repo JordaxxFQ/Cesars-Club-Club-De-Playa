2bea5030fc235dabe4f7e869d13b9ac6bcb7406d
¨ismaelofuentes¨ <ismaelofuentes@gmail.com>
2026-01-28 20:20:04 -0400

    asd

--- Cesars Club Club De Playa/Formularios/FrmPanelMesas.vb
+++ Cesars Club Club De Playa/Formularios/FrmPanelMesas.vb
@@ -1,11 +1,13 @@
 ﻿Imports System.Data.OleDb
+Imports Cesars_Club_Club_De_Playa.DAL
 
 Public Class FrmPanelMesas
     ' Tu cadena de conexión habitual
-    Dim ruta As String = IO.Path.Combine(Application.StartupPath, "DataBase", "BD Proyecto Final.accdb")
+    Dim ruta As String = IO.Path.GetFullPath(IO.Path.Combine(Application.StartupPath, "..\..\..\DataBase\BD Proyecto Final.accdb"))
     Dim connectionString As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" & ruta
 
     Private Sub FrmPanelMesas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        enlace()
         CargarMesas()
     End Sub
 
