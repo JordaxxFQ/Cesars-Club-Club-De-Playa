e59c25e47f9551d085cd9c7d20a87ee90400a938
¨ismaelofuentes¨ <ismaelofuentes@gmail.com>
2026-01-28 20:36:38 -0400

    asd

--- Cesars Club Club De Playa/Formularios/FrmDetalleMesa.Designer.vb
+++ Cesars Club Club De Playa/Formularios/FrmDetalleMesa.Designer.vb
@@ -22,9 +22,73 @@ Partial Class FrmDetalleMesa
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "FrmDetalleMesa"
+        txtCedula = New TextBox()
+        txtNombre = New TextBox()
+        btnBuscarCliente = New Button()
+        btnGuardar = New Button()
+        lblTitulo = New Label()
+        SuspendLayout()
+        ' 
+        ' txtCedula
+        ' 
+        txtCedula.Location = New Point(87, 81)
+        txtCedula.Name = "txtCedula"
+        txtCedula.Size = New Size(100, 23)
+        txtCedula.TabIndex = 0
+        ' 
+        ' txtNombre
+        ' 
+        txtNombre.Location = New Point(87, 151)
+        txtNombre.Name = "txtNombre"
+        txtNombre.Size = New Size(100, 23)
+        txtNombre.TabIndex = 1
+        ' 
+        ' btnBuscarCliente
+        ' 
+        btnBuscarCliente.Location = New Point(369, 228)
+        btnBuscarCliente.Name = "btnBuscarCliente"
+        btnBuscarCliente.Size = New Size(75, 23)
+        btnBuscarCliente.TabIndex = 2
+        btnBuscarCliente.Text = "Button1"
+        btnBuscarCliente.UseVisualStyleBackColor = True
+        ' 
+        ' btnGuardar
+        ' 
+        btnGuardar.Location = New Point(369, 165)
+        btnGuardar.Name = "btnGuardar"
+        btnGuardar.Size = New Size(75, 23)
+        btnGuardar.TabIndex = 3
+        btnGuardar.Text = "Button2"
+        btnGuardar.UseVisualStyleBackColor = True
+        ' 
+        ' lblTitulo
+        ' 
+        lblTitulo.AutoSize = True
+        lblTitulo.Location = New Point(87, 32)
+        lblTitulo.Name = "lblTitulo"
+        lblTitulo.Size = New Size(50, 15)
+        lblTitulo.TabIndex = 4
+        lblTitulo.Text = "lblTitulo"
+        ' 
+        ' FrmDetalleMesa
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(lblTitulo)
+        Controls.Add(btnGuardar)
+        Controls.Add(btnBuscarCliente)
+        Controls.Add(txtNombre)
+        Controls.Add(txtCedula)
+        Name = "FrmDetalleMesa"
+        Text = "FrmDetalleMesa"
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents txtCedula As TextBox
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents btnBuscarCliente As Button
+    Friend WithEvents btnGuardar As Button
+    Friend WithEvents lblTitulo As Label
 End Class
--- Cesars Club Club De Playa/Formularios/FrmDetalleMesa.vb
+++ Cesars Club Club De Playa/Formularios/FrmDetalleMesa.vb
@@ -1,3 +1,48 @@
-﻿Public Class FrmDetalleMesa
+﻿Imports System.Data.OleDb
+Imports Cesars_Club_Club_De_Playa.DAL
+Public Class FrmDetalleMesa
+    Dim _idMesa As Integer
+
+    Dim ruta As String = IO.Path.GetFullPath(IO.Path.Combine(Application.StartupPath, "..\..\..\DataBase\BD Proyecto Final.accdb"))
+    Dim connectionString As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" & ruta
+    ' Constructor personalizado para recibir el ID
+    Public Sub New(idMesa As Integer)
+        InitializeComponent()
+        _idMesa = idMesa
+    End Sub
+
+    Private Sub FrmDetalleMesa_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lblTitulo.Text = "Gestionando Mesa ID: " & _idMesa
+        ' Aquí podrías consultar la BD para ver si la mesa ya tiene una reserva activa
+        ' y llenar los datos automáticamente.
+    End Sub
+
+    ' Botón para buscar cliente por Cédula (Como lo pediste)
+    Private Sub btnBuscarCliente_Click(sender As Object, e As EventArgs) Handles btnBuscarCliente.Click
+        Dim cedula As String = txtCedula.Text
+        ' Haces un SELECT * FROM Clientes WHERE Cedula = ...
+        ' Si existe, llenas txtNombre.Text
+        ' Si no, le dices que lo registre.
+    End Sub
+
+    ' Botón Confirmar Reserva / Ocupar
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+        ' 1. INSERTAR en la tabla Reservas o Pedidos
+        ' INSERT INTO Reservas (ID_Mesa, ID_Cliente, EstadoReserva...) VALUES (_idMesa, ...)
+
+        ' 2. ACTUALIZAR el estado de la mesa en Zonas
+        ' UPDATE Zonas SET Estado = 'Ocupada' WHERE ID_Mesa = _idMesa
+
+        ActualizarEstadoMesa("Ocupada") ' Llamas a un sub que haga el UPDATE
+
+        MessageBox.Show("Mesa asignada correctamente")
+        Me.Close()
+    End Sub
+
+    Private Sub ActualizarEstadoMesa(nuevoEstado As String)
+        ' Código SQL UPDATE Zonas SET Estado = @estado WHERE ID_Mesa = @id...
+        ' Usando _idMesa y nuevoEstado
+        ' End Using
+    End Sub
 
 End Class
