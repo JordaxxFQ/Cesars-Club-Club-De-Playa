97fc2cfc81519f24e8835c471dcc67d22b6f9353
¨ismaelofuentes¨ <ismaelofuentes@gmail.com>
2026-01-31 22:17:21 -0400

    asd

--- Cesars Club Club De Playa/Formularios/FrmAggPerso.vb
+++ Cesars Club Club De Playa/Formularios/FrmAggPerso.vb
@@ -4,6 +4,7 @@ Public Class FrmAggPerso
 
     Dim ruta As String = IO.Path.GetFullPath(IO.Path.Combine(Application.StartupPath, "..\..\..\DataBase\BD Proyecto Final.accdb"))
     Dim connectionString As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" & ruta
+    Dim idPersonalSeleccionado As Integer = 0
     Private Sub FrmAggPerso_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         cmbRol.Items.Add("Gerente")
         cmbRol.Items.Add("Cocinero")
@@ -12,6 +13,13 @@ Public Class FrmAggPerso
         CargarTurno()
         CargarDatos()
 
+    End Sub
+    Private Sub LimpiarCampos()
+        txtboxusuario.Clear()
+        cmbRol.SelectedIndex = -1
+        cmbTurno.SelectedIndex = -1
+        txtboxContra.Clear()
+        idPersonalSeleccionado = 0
     End Sub
 
     Private Sub CargarTurno()
@@ -48,18 +56,34 @@ Public Class FrmAggPerso
                 Dim adaptador As New OleDbDataAdapter(query, conexion)
                 Dim dataset As New DataSet()
 
+                adaptador.Fill(dataset, "TablaAggPersonal")
+                DgvPersonal.DataSource = dataset.Tables("TablaAggPersonal")
 
-                adaptador.Fill(dataset, "TablaAgg")
-                DataGridView1.DataSource = dataset.Tables("TablaAgg")
+                DgvPersonal.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
+                DgvPersonal.ReadOnly = True
+                DgvPersonal.AllowUserToAddRows = False
+                DgvPersonal.AutoGenerateColumns = True
 
-                DataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
-                DataGridView1.ReadOnly = True
-                DataGridView1.AllowUserToAddRows = False
-                DataGridView1.AutoGenerateColumns = True
+                If DgvPersonal.Columns.Contains("ID_Personal") Then
+                    DgvPersonal.Columns("ID_Personal").Visible = False
+                End If
+
             Catch ex As Exception
                 MessageBox.Show("Error al cargar datos: " & ex.Message)
             End Try
         End Using
+    End Sub
+    Private Sub DgvPersonal_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DgvPersonal.CellClick
+        If e.RowIndex >= 0 Then
+            Dim fila As DataGridViewRow = DgvPersonal.Rows(e.RowIndex)
+            idPersonalSeleccionado = CInt(fila.Cells("ID_Personal").Value)
+
+            txtboxusuario.Text = fila.Cells("Usuario").Value.ToString()
+            txtboxContra.Text = fila.Cells("Contraseña").Value.ToString()
+            cmbRol.Text = fila.Cells("ID_Rol").Value.ToString()
+            cmbTurno.Text = fila.Cells("Turno").Value.ToString()
+
+        End If
     End Sub
 
     Private Sub btnConfirmar_Click(sender As Object, e As EventArgs) Handles btnConfirmar.Click
@@ -69,14 +93,11 @@ Public Class FrmAggPerso
             Exit Sub
         End If
 
-
-
         Dim query As String = "INSERT INTO Personal (Usuario, Contraseña, ID_Rol, Turno) VALUES (?, ?, ?, ?)"
 
         Using conexion As New OleDbConnection(connectionString)
             Try
                 Dim comando As New OleDbCommand(query, conexion)
-
 
                 comando.Parameters.AddWithValue("@usuario", txtboxusuario.Text)
                 comando.Parameters.AddWithValue("@contra", txtboxContra.Text)
@@ -96,6 +117,39 @@ Public Class FrmAggPerso
 
             Catch ex As Exception
                 MessageBox.Show("Error al guardar: " & ex.Message)
+            End Try
+        End Using
+    End Sub
+
+    Private Sub btnEdit_Click(sender As Object, e As EventArgs) Handles btnEdit.Click
+        If idPersonalSeleccionado = 0 Then
+            MessageBox.Show("Seleccione el Personal a modificar.")
+            Exit Sub
+        End If
+
+        Dim query As String = "UPDATE Personal SET Usuario=?, Contraseña=?, ID_Rol=?, Turno=? WHERE ID_Personal=?"
+
+        Using conexion As New OleDbConnection(connectionString)
+            Try
+                conexion.Open()
+                Dim cmd As New OleDbCommand(query, conexion)
+
+                cmd.Parameters.AddWithValue("@usuario", txtboxusuario.Text)
+                cmd.Parameters.AddWithValue("@contra", txtboxContra.Text)
+                cmd.Parameters.AddWithValue("@Rol", cmbRol.Text)
+                cmd.Parameters.AddWithValue("@turno", cmbTurno.Text)
+
+                cmd.Parameters.Add("@id", OleDbType.Integer).Value = idPersonalSeleccionado
+
+                cmd.ExecuteNonQuery()
+                MessageBox.Show("Producto actualizado con éxito, incluyendo estado de venta.")
+
+                CargarDatos()
+                LimpiarCampos()
+
+
+            Catch ex As Exception
+                MessageBox.Show("Error al modificar: " & ex.Message)
             End Try
         End Using
     End Sub
