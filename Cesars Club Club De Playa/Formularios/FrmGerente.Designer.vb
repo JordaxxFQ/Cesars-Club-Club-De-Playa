724b0af182516e11962a11aefe0e8f92cf0569e7
¨ismaelofuentes¨ <ismaelofuentes@gmail.com>
2026-02-09 15:35:20 -0400

    asd

--- Cesars Club Club De Playa/Formularios/FrmGerente.Designer.vb
+++ Cesars Club Club De Playa/Formularios/FrmGerente.Designer.vb
@@ -22,28 +22,30 @@ Partial Class FrmGerente
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        btnRegistro = New Button()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmGerente))
         btnReservaciones = New Button()
         btnMesita = New Button()
         btnFactura = New Button()
         btonPedido = New Button()
         btnClient = New Button()
-        btnProducto = New Button()
         BtnCocina = New Button()
+        PtbProducto = New PictureBox()
+        LblProducto = New Label()
+        PtbPersonal = New PictureBox()
+        LblPersonal = New Label()
+        PtbClientes = New PictureBox()
+        LblClientes = New Label()
+        LblPedido = New Label()
+        PtbPedidos = New PictureBox()
+        CType(PtbProducto, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PtbPersonal, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PtbClientes, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PtbPedidos, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
-        ' 
-        ' btnRegistro
-        ' 
-        btnRegistro.Location = New Point(19, 28)
-        btnRegistro.Name = "btnRegistro"
-        btnRegistro.Size = New Size(117, 98)
-        btnRegistro.TabIndex = 0
-        btnRegistro.Text = "Personal"
-        btnRegistro.UseVisualStyleBackColor = True
         ' 
         ' btnReservaciones
         ' 
-        btnReservaciones.Location = New Point(20, 446)
+        btnReservaciones.Location = New Point(882, 157)
         btnReservaciones.Name = "btnReservaciones"
         btnReservaciones.Size = New Size(117, 98)
         btnReservaciones.TabIndex = 1
@@ -52,7 +54,7 @@ Partial Class FrmGerente
         ' 
         ' btnMesita
         ' 
-        btnMesita.Location = New Point(142, 234)
+        btnMesita.Location = New Point(882, 461)
         btnMesita.Name = "btnMesita"
         btnMesita.Size = New Size(117, 98)
         btnMesita.TabIndex = 2
@@ -61,7 +63,7 @@ Partial Class FrmGerente
         ' 
         ' btnFactura
         ' 
-        btnFactura.Location = New Point(142, 28)
+        btnFactura.Location = New Point(826, 336)
         btnFactura.Name = "btnFactura"
         btnFactura.Size = New Size(118, 98)
         btnFactura.TabIndex = 3
@@ -70,7 +72,7 @@ Partial Class FrmGerente
         ' 
         ' btonPedido
         ' 
-        btonPedido.Location = New Point(19, 336)
+        btonPedido.Location = New Point(998, 308)
         btonPedido.Margin = New Padding(3, 2, 3, 2)
         btonPedido.Name = "btonPedido"
         btonPedido.Size = New Size(118, 98)
@@ -80,7 +82,7 @@ Partial Class FrmGerente
         ' 
         ' btnClient
         ' 
-        btnClient.Location = New Point(19, 131)
+        btnClient.Location = New Point(642, 308)
         btnClient.Margin = New Padding(3, 2, 3, 2)
         btnClient.Name = "btnClient"
         btnClient.Size = New Size(118, 98)
@@ -88,19 +90,9 @@ Partial Class FrmGerente
         btnClient.Text = "Clientes"
         btnClient.UseVisualStyleBackColor = True
         ' 
-        ' btnProducto
-        ' 
-        btnProducto.Location = New Point(143, 131)
-        btnProducto.Margin = New Padding(3, 2, 3, 2)
-        btnProducto.Name = "btnProducto"
-        btnProducto.Size = New Size(118, 98)
-        btnProducto.TabIndex = 6
-        btnProducto.Text = "Productos"
-        btnProducto.UseVisualStyleBackColor = True
-        ' 
         ' BtnCocina
         ' 
-        BtnCocina.Location = New Point(19, 234)
+        BtnCocina.Location = New Point(651, 461)
         BtnCocina.Margin = New Padding(3, 2, 3, 2)
         BtnCocina.Name = "BtnCocina"
         BtnCocina.Size = New Size(118, 98)
@@ -108,31 +100,128 @@ Partial Class FrmGerente
         BtnCocina.Text = "Cocina"
         BtnCocina.UseVisualStyleBackColor = True
         ' 
+        ' PtbProducto
+        ' 
+        PtbProducto.Image = CType(resources.GetObject("PtbProducto.Image"), Image)
+        PtbProducto.Location = New Point(289, 234)
+        PtbProducto.Name = "PtbProducto"
+        PtbProducto.Size = New Size(113, 89)
+        PtbProducto.SizeMode = PictureBoxSizeMode.Zoom
+        PtbProducto.TabIndex = 8
+        PtbProducto.TabStop = False
+        ' 
+        ' LblProducto
+        ' 
+        LblProducto.AutoSize = True
+        LblProducto.Font = New Font("Segoe Print", 10F, FontStyle.Bold)
+        LblProducto.Location = New Point(395, 250)
+        LblProducto.Name = "LblProducto"
+        LblProducto.Size = New Size(81, 24)
+        LblProducto.TabIndex = 9
+        LblProducto.Text = "Productos"
+        ' 
+        ' PtbPersonal
+        ' 
+        PtbPersonal.Image = CType(resources.GetObject("PtbPersonal.Image"), Image)
+        PtbPersonal.Location = New Point(287, 44)
+        PtbPersonal.Name = "PtbPersonal"
+        PtbPersonal.Size = New Size(115, 89)
+        PtbPersonal.SizeMode = PictureBoxSizeMode.Zoom
+        PtbPersonal.TabIndex = 10
+        PtbPersonal.TabStop = False
+        ' 
+        ' LblPersonal
+        ' 
+        LblPersonal.AutoSize = True
+        LblPersonal.Font = New Font("Segoe Print", 10F, FontStyle.Bold)
+        LblPersonal.Location = New Point(400, 87)
+        LblPersonal.Name = "LblPersonal"
+        LblPersonal.Size = New Size(69, 24)
+        LblPersonal.TabIndex = 11
+        LblPersonal.Text = "Personal"
+        ' 
+        ' PtbClientes
+        ' 
+        PtbClientes.Image = CType(resources.GetObject("PtbClientes.Image"), Image)
+        PtbClientes.Location = New Point(287, 139)
+        PtbClientes.Name = "PtbClientes"
+        PtbClientes.Size = New Size(115, 89)
+        PtbClientes.SizeMode = PictureBoxSizeMode.Zoom
+        PtbClientes.TabIndex = 12
+        PtbClientes.TabStop = False
+        ' 
+        ' LblClientes
+        ' 
+        LblClientes.AutoSize = True
+        LblClientes.Font = New Font("Segoe Print", 10F, FontStyle.Bold)
+        LblClientes.Location = New Point(395, 176)
+        LblClientes.Name = "LblClientes"
+        LblClientes.Size = New Size(64, 24)
+        LblClientes.TabIndex = 13
+        LblClientes.Text = "Clientes"
+        ' 
+        ' LblPedido
+        ' 
+        LblPedido.AutoSize = True
+        LblPedido.Font = New Font("Segoe Print", 10F, FontStyle.Bold)
+        LblPedido.Location = New Point(395, 345)
+        LblPedido.Name = "LblPedido"
+        LblPedido.Size = New Size(63, 24)
+        LblPedido.TabIndex = 15
+        LblPedido.Text = "Pedidos"
+        ' 
+        ' PtbPedidos
+        ' 
+        PtbPedidos.Image = CType(resources.GetObject("PtbPedidos.Image"), Image)
+        PtbPedidos.Location = New Point(289, 336)
+        PtbPedidos.Name = "PtbPedidos"
+        PtbPedidos.Size = New Size(113, 89)
+        PtbPedidos.SizeMode = PictureBoxSizeMode.Zoom
+        PtbPedidos.TabIndex = 14
+        PtbPedidos.TabStop = False
+        ' 
         ' FrmGerente
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         BackColor = SystemColors.GradientActiveCaption
-        ClientSize = New Size(891, 556)
+        ClientSize = New Size(1279, 622)
+        Controls.Add(LblPedido)
+        Controls.Add(PtbPedidos)
+        Controls.Add(LblClientes)
+        Controls.Add(LblPersonal)
+        Controls.Add(LblProducto)
         Controls.Add(BtnCocina)
-        Controls.Add(btnProducto)
         Controls.Add(btnClient)
         Controls.Add(btonPedido)
         Controls.Add(btnFactura)
         Controls.Add(btnMesita)
         Controls.Add(btnReservaciones)
-        Controls.Add(btnRegistro)
+        Controls.Add(PtbClientes)
+        Controls.Add(PtbPersonal)
+        Controls.Add(PtbProducto)
         Name = "FrmGerente"
+        StartPosition = FormStartPosition.CenterScreen
         Text = "Menú"
+        CType(PtbProducto, ComponentModel.ISupportInitialize).EndInit()
+        CType(PtbPersonal, ComponentModel.ISupportInitialize).EndInit()
+        CType(PtbClientes, ComponentModel.ISupportInitialize).EndInit()
+        CType(PtbPedidos, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
+        PerformLayout()
     End Sub
-
-    Friend WithEvents btnRegistro As Button
     Friend WithEvents btnReservaciones As Button
     Friend WithEvents btnMesita As Button
     Friend WithEvents btnFactura As Button
     Friend WithEvents btonPedido As Button
     Friend WithEvents btnClient As Button
-    Friend WithEvents btnProducto As Button
     Friend WithEvents BtnCocina As Button
+    Friend WithEvents PtbProducto As PictureBox
+    Friend WithEvents LblProducto As Label
+    Friend WithEvents PtbPersonal As PictureBox
+    Friend WithEvents LblPersonal As Label
+    Friend WithEvents PtbClientes As PictureBox
+    Friend WithEvents LblClientes As Label
+    Friend WithEvents LblPedido As Label
+    Friend WithEvents PtbPedidos As PictureBox
 End Class
--- Cesars Club Club De Playa/Formularios/FrmGerente.vb
+++ Cesars Club Club De Playa/Formularios/FrmGerente.vb
@@ -1,42 +1,61 @@
 ﻿Public Class FrmGerente
-    Private Sub FrmGerente_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    Private estadosOriginales As New Dictionary(Of Control, Rectangle)
+    Private Const crecimiento As Integer = 15
+    Private Sub FrmGerente_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        RegistrarEstadoOriginal(PtbClientes)
+        RegistrarEstadoOriginal(PtbPersonal)
+        RegistrarEstadoOriginal(PtbProducto)
+        RegistrarEstadoOriginal(PtbPedidos)
     End Sub
-    Private Sub btnRegistro_Click(sender As Object, e As EventArgs) Handles btnRegistro.Click
+
+    Private Sub RegistrarEstadoOriginal(pb As PictureBox)
+        If Not estadosOriginales.ContainsKey(pb) Then
+            estadosOriginales.Add(pb, pb.Bounds)
+            pb.SizeMode = PictureBoxSizeMode.Zoom
+        End If
+    End Sub
+    Private Sub Efecto_MouseEnter(sender As Object, e As EventArgs) Handles PtbClientes.MouseEnter, PtbPersonal.MouseEnter, PtbProducto.MouseEnter
+        ' El "sender" es el PictureBox que activó el evento
+        Dim pb As PictureBox = DirectCast(sender, PictureBox)
+        Dim rectOriginal As Rectangle = estadosOriginales(pb)
+
+        ' Aplicamos el crecimiento desde el centro
+        pb.SetBounds(rectOriginal.X - (crecimiento \ 2),
+                 rectOriginal.Y - (crecimiento \ 2),
+                 rectOriginal.Width + crecimiento,
+                 rectOriginal.Height + crecimiento)
+    End Sub
+
+    Private Sub Efecto_MouseLeave(sender As Object, e As EventArgs) Handles PtbClientes.MouseLeave, PtbPersonal.MouseLeave, PtbProducto.MouseLeave
+        ' El "sender" nos dice cuál restaurar
+        Dim pb As PictureBox = DirectCast(sender, PictureBox)
+        Dim rectOriginal As Rectangle = estadosOriginales(pb)
+
+        ' Restauramos sus valores originales exactos
+        pb.Bounds = rectOriginal
+    End Sub
+    Private Sub PtbPersonal_Click_1(sender As Object, e As EventArgs) Handles PtbPersonal.Click, LblPersonal.Click
         FrmRegistroPersonal.Show()
     End Sub
 
-    Private Sub btnClient_Click(sender As Object, e As EventArgs) Handles btnClient.Click
+    Private Sub PtbCliente_Click(sender As Object, e As EventArgs) Handles PtbClientes.Click, LblClientes.Click
         FrmRegistroClientes.Show()
     End Sub
 
-    Private Sub btnMesita_Click(sender As Object, e As EventArgs) Handles btnMesita.Click
-        FrmPanelMesas.Show()
+    Private Sub PtbProducto_Click(sender As Object, e As EventArgs) Handles PtbProducto.Click, LblProducto.Click
+        FrmProductos.Show()
+    End Sub
+    Private Sub PtbPedidos_Click(sender As Object, e As EventArgs) Handles PtbPedidos.Click, LblPedido.Click
+        FrmPedidos.Show()
+    End Sub
+
+    Private Sub FrmGerente_FormClosed(sender As Object, e As FormClosedEventArgs) Handles Me.FormClosed
+
     End Sub
     Private Sub FrmGerente_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
         Application.Exit()
     End Sub
 
-    Private Sub btnReservaciones_Click(sender As Object, e As EventArgs) Handles btnReservaciones.Click
-        FrmReserva.Show()
-    End Sub
 
-    Private Sub btonPedido_Click(sender As Object, e As EventArgs) Handles btonPedido.Click
-        FrmPedidos.Show()
-    End Sub
-
-    Private Sub BtnCocina_Click(sender As Object, e As EventArgs) Handles BtnCocina.Click
-        FrmCocina.Show()
-    End Sub
-
-    Private Sub btnProducto_Click(sender As Object, e As EventArgs) Handles btnProducto.Click
-        FrmProductos.Show()
-    End Sub
-
-    Private Sub btnFactura_Click(sender As Object, e As EventArgs) Handles btnFactura.Click
-        FrmFactura.Show()
-    End Sub
-    Private Sub FrmGerente_FormClosed(sender As Object, e As FormClosedEventArgs) Handles Me.FormClosed
-
-    End Sub
 End Class
